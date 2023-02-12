a660e9ca3c24382528e856deae1b46f42e6a64a5
Eduard Muli <em48105@ubt-uni.net>
2023-02-12 22:23:18 +0100

    21

--- projekti.sql
+++ projekti.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2023 at 09:54 PM
+-- Generation Time: Feb 12, 2023 at 10:19 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -99,35 +99,7 @@ INSERT INTO `produktet` (`ID`, `foto_produktit`, `emri_produktit`, `cmimi_produk
 (44, './img/wproductt4.jpeg', 'Floral Devore Tie Waist Blazer', '39.99$', 'womens_clothes', 'Eduard'),
 (45, './img/wproductt6.jpeg', 'Faux Leather Heart Bandeau Top', '39.99$', 'womens_clothes', 'Eduard'),
 (46, './img/wproductt8.jpeg', 'Ombre Cropped Sweater Vest', '79.99$', 'womens_clothes', 'Eduard'),
-(47, './img/wproductt9.jpeg', 'Mid Rise Tie Hem Twill Cargo', '29.99$', 'womens_clothes', 'Eduard'),
-(48, './img/wproductt10.jpeg', 'Faux Leather Boots', '33.99$', 'womens_clothes', 'Eduard'),
-(49, './img/wproductt11.jpeg', 'Two Tone Boots', '39.99$', 'womens_clothes', 'Eduard'),
-(50, './img/wproductt12.jpeg', 'Calf High Block Heel', '32.00$', 'womens_clothes', 'Eduard'),
-(51, './img/productt3-men.jpg', 'The North Face', '69.99$', 'mens_clothes', 'Eduard'),
-(52, './img/productt4-men.jpg', '\'47 New York HAT', '129.99$', 'mens_clothes', 'Eduard'),
-(53, './img/productt6-men.jpg', 'Polo Ralph', '109.99$', 'mens_clothes', 'Eduard'),
-(54, './img/productt7-men.jpg', '’47 UO HAT', '49.99$', 'mens_clothes', 'Eduard'),
-(55, './img/productt8-men.jpg', 'Technical Cargo Jogger', '99.99$', 'mens_clothes', 'Eduard'),
-(56, './img/productt9-men.jpg', 'Boxy Polo Shirt', '50.99$', 'mens_clothes', 'Eduard'),
-(57, './img/productt10-men.jpg', 'Faux Wool Overshirt', '99.99$', 'mens_clothes', 'Eduard'),
-(59, './img/productt12-men.jpg', 'Leather Shirt Jacket', '69.99$', 'mens_clothes', 'Eduard'),
-(60, './img/productt13-men.jpg', 'Cloth Shortstop Tee', '49.99$', 'mens_clothes', 'Eduard'),
-(61, './img/productt1-kids.jpg', 'Shirt and Joggers Set', '59.99$', 'kids_clothes', 'Eduard'),
-(62, './img/productt2-kids.jpg', 'Extra-soft Joggers', '59.99$', 'kids_clothes', 'Eduard'),
-(63, './img/productt5-kids.jpg', '2-pack Sweatshirts', '89.99$', 'kids_clothes', 'Eduard'),
-(64, './img/productt6-kids.jpg', '2-pack Sweatshirts', '109.99$', 'kids_clothes', 'Eduard'),
-(65, './img/productt7-kids.jpg', 'Extra-soft Joggers', '49.99$', 'kids_clothes', 'Eduard'),
-(66, './img/productt8-kids.jpg', '2-piece Sweatshirt Set', '99.99$', 'kids_clothes', 'Eduard'),
-(67, './img/productt9-kids.jpg', '2-piece Top and Leggings Set', '50.99$', 'kids_clothes', 'Eduard'),
-(68, './img/productt10-kids.jpg', 'Hoodie', '39.99$', 'kids_clothes', 'Eduard'),
-(69, './img/productt11-kids.jpg', '2-piece Velour Set', '89.99$', 'kids_clothes', 'Eduard'),
-(70, './img/productt13-kids.jpg', '2-piece set', '69.99$', 'kids_clothes', 'Eduard'),
-(71, './img/bproductt1.jpeg', 'MEON', '43.99$', 'beauty', 'Eduard'),
-(72, './img/bproductt3.jpeg', 'FEON', '24.99$', 'beauty', 'Eduard'),
-(73, './img/bproductt4.jpeg', 'LEON', '49.99$', 'beauty', 'Eduard'),
-(74, './img/bproductt5.jpeg', 'DEON', '35.99$', 'beauty', 'Eduard'),
-(75, './img/bproductt7.jpeg', 'TEON', '26.99$', 'beauty', 'Eduard'),
-(76, './img/bproductt8.jpeg', 'PEON', '34.99$', 'beauty', 'Eduard');
+(51, './img/productt3-men.jpg', 'The North Face', '69.99$', 'mens_clothes', 'Eduard');
 
 -- --------------------------------------------------------
 
